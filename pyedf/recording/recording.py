fcc3b71463970c91a0f9c8c87d2889f15c5af54e
Justus Schwabedal <jschwabedal@gmail.com>
2016-11-08 13:49:51 +0100

    Indented all files.

--- pyedf/recording/recording.py
+++ pyedf/recording/recording.py
@@ -12,131 +12,131 @@ import datetime
 
 class recording(edf_hdr_struct):
 
-	def __init__(self, filename, md5checksum=None, verbose=0):
-		edf_hdr_struct.__init__(self, filename, md5checksum=md5checksum)
+    def __init__(self, filename, md5checksum=None, verbose=0):
+        edf_hdr_struct.__init__(self, filename, md5checksum=md5checksum)
 
-		self.verbose = verbose
+        self.verbose = verbose
 
-		if self.verbose:
-			print("recording: Opening edf file", filename)
-
-
-	def select_channels(self, channels=None):
-
-		if channels == None:	# Select all channels.
-			return np.arange(self.edfsignals)
-		
-		elif not np.iterable(channels) or type(channels) == np.typeDict['str'] or type(channels) == str:	# Wenn channels nicht indizierbar oder ein String ist, ..
-			channels = [channels]										 #				.. dann mach ne Liste draus.
-
-		channelindices = []
-		for channel in channels:
-
-			if type(channel) == np.typeDict['int'] or type(channel) == int:
-				channelindices.append( channel )
-
-			elif type(channel) == np.typeDict['str'] or type(channel) == str:
-
-				if channeltypes.is_channeltype(channel):									# if it is a channel type : 
-					channelindices.extend( np.arange(self.edfsignals)[self.channeltypes == channel] )	#		.. load all the channels.
-
-				else:
-					try:
-						channelindices.append( self.channelnames.index(channel) )
-					except:
-						print("channel '%s' not in List." % (channel))
-						print("Channels :", self.channelnames)
-						raise AttributeError
+        if self.verbose:
+            print("recording: Opening edf file", filename)
 
 
-		if len(channelindices) == 0:
-			print("channellist empty.  Channels '%s' not understood." % (channels))
-			print("Possible Channels :", self.channelnames)
-			raise AttributeError
+    def select_channels(self, channels=None):
 
-		return channelindices
+        if channels == None:    # Select all channels.
+            return np.arange(self.edfsignals)
+        
+        elif not np.iterable(channels) or type(channels) == np.typeDict['str'] or type(channels) == str:    # Wenn channels nicht indizierbar oder ein String ist, ..
+            channels = [channels]                                         #                .. dann mach ne Liste draus.
 
+        channelindices = []
+        for channel in channels:
 
-		# Setup the channel information ..
-		if not (channels == None or np.iterable(channels)):
-			print("recording : Parameter 'channels' not understood.", channels)
-			raise AttributeError
+            if type(channel) == np.typeDict['int'] or type(channel) == int:
+                channelindices.append( channel )
 
+            elif type(channel) == np.typeDict['str'] or type(channel) == str:
 
-	def get_samplingrate(self, channels):
+                if channeltypes.is_channeltype(channel):                                    # if it is a channel type : 
+                    channelindices.extend( np.arange(self.edfsignals)[self.channeltypes == channel] )    #        .. load all the channels.
 
-		channels = self.select_channels(channels)
-
-		samplingrate = self.samplingrates[channels]
-
-		if np.iterable(samplingrate):
-
-			if not all(samplingrate == samplingrate[0]*np.ones((samplingrate.size), dtype=int)):	# If sampling rates are unqual ..
-				print("recording : Unqual sampling rates.", samplingrate)
-				return None
-
-			samplingrate = samplingrate[0]
-
-		return samplingrate
+                else:
+                    try:
+                        channelindices.append( self.channelnames.index(channel) )
+                    except:
+                        print("channel '%s' not in List." % (channel))
+                        print("Channels :", self.channelnames)
+                        raise AttributeError
 
 
-	def get_data(self, state_of_interest=None, start=None, end=None, duration=None, channels=None):
+        if len(channelindices) == 0:
+            print("channellist empty.  Channels '%s' not understood." % (channels))
+            print("Possible Channels :", self.channelnames)
+            raise AttributeError
 
-		channels = self.select_channels(channels)
-
-		# Check if all channels have the same sampling rate ..
-		samplingrate = recording.get_samplingrate(self, channels)
-					
-
-		# Load the time information ..
-		# Start
-		if isinstance(state_of_interest, score.state):
-			start = state_of_interest
-			duration = state_of_interest.duration
-
-		if isinstance(start, datetime.datetime):	# start is a datetime object
-			n_start = int((start-self.start).total_seconds() * samplingrate)
-
-		elif isinstance(start, float):	# start is in seconds
-			n_start = int(start * samplingrate)
-
-		else:
-			print("# start is not defined:", start)
-			raise ValueError
-
-		# n_duration
-		if isinstance(end, datetime.datetime):	# end is a datetime object
-			n_duration = int((end-self.start).total_seconds() * samplingrate) - n_start
-
-		if isinstance(duration, float):	# duration is given in seconds
-			n_duration = int(duration * float(samplingrate))
-
-		if self.verbose: print("# Loading from", start, "(= datapoint", n_start, "), ", n_duration, "datapoints of channels", channels)
-
-		return samplingrate, self.read_physical_samples(channels, start=n_start, size=n_duration)
+        return channelindices
 
 
-	def __str__(self):
-		string = '\n############## %s ################\n' % (self.patient)
+        # Setup the channel information ..
+        if not (channels == None or np.iterable(channels)):
+            print("recording : Parameter 'channels' not understood.", channels)
+            raise AttributeError
 
-		string +='start : %s\n' % (str(self.start))
-		string +='edfsignals : %i\n' % (self.edfsignals)
 
-		string += '\nSignal Information\n'
-		string += '------------------\n' 
-		for i in xrange(self.edfsignals):
-			string += 'Signal %i\n'				% (i)
-			string += 'Name : %s\n'				% (self.channelnames[i])
-			string += 'Type : %s\n'				% (self.channeltypes[i])
-			string += 'Sampling rate : %i Hz\n' 		% (self.samplingrates[i])
+    def get_samplingrate(self, channels):
 
-			string += '\n'
-		string += '------------------\n'
-		
+        channels = self.select_channels(channels)
 
-		string += '############## %s ################\n' % ('#'*len(self.patient))
+        samplingrate = self.samplingrates[channels]
 
-		return string
+        if np.iterable(samplingrate):
+
+            if not all(samplingrate == samplingrate[0]*np.ones((samplingrate.size), dtype=int)):    # If sampling rates are unqual ..
+                print("recording : Unqual sampling rates.", samplingrate)
+                return None
+
+            samplingrate = samplingrate[0]
+
+        return samplingrate
+
+
+    def get_data(self, state_of_interest=None, start=None, end=None, duration=None, channels=None):
+
+        channels = self.select_channels(channels)
+
+        # Check if all channels have the same sampling rate ..
+        samplingrate = recording.get_samplingrate(self, channels)
+                    
+
+        # Load the time information ..
+        # Start
+        if isinstance(state_of_interest, score.state):
+            start = state_of_interest
+            duration = state_of_interest.duration
+
+        if isinstance(start, datetime.datetime):    # start is a datetime object
+            n_start = int((start-self.start).total_seconds() * samplingrate)
+
+        elif isinstance(start, float):    # start is in seconds
+            n_start = int(start * samplingrate)
+
+        else:
+            print("# start is not defined:", start)
+            raise ValueError
+
+        # n_duration
+        if isinstance(end, datetime.datetime):    # end is a datetime object
+            n_duration = int((end-self.start).total_seconds() * samplingrate) - n_start
+
+        if isinstance(duration, float):    # duration is given in seconds
+            n_duration = int(duration * float(samplingrate))
+
+        if self.verbose: print("# Loading from", start, "(= datapoint", n_start, "), ", n_duration, "datapoints of channels", channels)
+
+        return samplingrate, self.read_physical_samples(channels, start=n_start, size=n_duration)
+
+
+    def __str__(self):
+        string = '\n############## %s ################\n' % (self.patient)
+
+        string +='start : %s\n' % (str(self.start))
+        string +='edfsignals : %i\n' % (self.edfsignals)
+
+        string += '\nSignal Information\n'
+        string += '------------------\n' 
+        for i in xrange(self.edfsignals):
+            string += 'Signal %i\n'				% (i)
+            string += 'Name : %s\n'				% (self.channelnames[i])
+            string += 'Type : %s\n'				% (self.channeltypes[i])
+            string += 'Sampling rate : %i Hz\n' 		% (self.samplingrates[i])
+
+            string += '\n'
+        string += '------------------\n'
+        
+
+        string += '############## %s ################\n' % ('#'*len(self.patient))
+
+        return string
 
 
 
@@ -147,25 +147,25 @@ class recording(edf_hdr_struct):
 
 if __name__ == "__main__":
 
-	import pylab
+    import pylab
 
-	START = 0
-	SIZE = 100000
-	ch = pylab.array([1, 15, 1], dtype=long)
-	dur = 200.
-
-
-	rec = recording(filename="/home/jus/Data/capslpdb/n4/n4.edf", verbose=0)
-	fs, data = rec.get_data(start=rec.start, duration=dur, channels='EEG')
-	t = np.arange(data.shape[1])/float(fs)
-	print(fs)
-	exit(0)
-
-	offset = 80
-	for (j, dat_j) in enumerate(data):
-		pylab.plot(t, dat_j-offset*j, 'k-')
-
-	pylab.show()
+    START = 0
+    SIZE = 100000
+    ch = pylab.array([1, 15, 1], dtype=long)
+    dur = 200.
 
 
-	del rec
+    rec = recording(filename="/home/jus/Data/capslpdb/n4/n4.edf", verbose=0)
+    fs, data = rec.get_data(start=rec.start, duration=dur, channels='EEG')
+    t = np.arange(data.shape[1])/float(fs)
+    print(fs)
+    exit(0)
+
+    offset = 80
+    for (j, dat_j) in enumerate(data):
+        pylab.plot(t, dat_j-offset*j, 'k-')
+
+    pylab.show()
+
+
+    del rec
--- pyedf/score/score.py
+++ pyedf/score/score.py
@@ -10,168 +10,168 @@ import re
 
 class Score(object):
 
-	commentSymbol = '#'	# used for comments in state.annot
-	lineSeparator = ','	# used as separators in the line
-	states_dict = dict()
+    commentSymbol = '#'    # used for comments in state.annot
+    lineSeparator = ','    # used as separators in the line
+    states_dict = dict()
 
-	def __init__(self, filename=None, states=None, verbose=0):
+    def __init__(self, filename=None, states=None, verbose=0):
 
-		self.verbose = verbose
-		self.states  = states
+        self.verbose = verbose
+        self.states  = states
 
-		if filename:
+        if filename:
 
-			if os.path.exists(filename) == False:
-				print("Score file %s does not exist." % (filename))
-				raise AttributeError
+            if os.path.exists(filename) == False:
+                print("Score file %s does not exist." % (filename))
+                raise AttributeError
 
-			self.states = self.load(filename)
-			if self.verbose > 0: print("score: score file '%s' found." % (filename))
-			if self.verbose == 2: print("score: the states", self.states)
+            self.states = self.load(filename)
+            if self.verbose > 0: print("score: score file '%s' found." % (filename))
+            if self.verbose == 2: print("score: the states", self.states)
 
-		else:
-			if self.verbose: print("# score: no score file given.")
+        else:
+            if self.verbose: print("# score: no score file given.")
                         self.states = []
 
-		#if np.iterable(self.states) :
-		#	self.states = np.sort(states)
-		#	self.interpret_states()
+        #if np.iterable(self.states) :
+        #    self.states = np.sort(states)
+        #    self.interpret_states()
 
 
-	def interpret_states(self):
-		pass
+    def interpret_states(self):
+        pass
 
 
-	def isComment(self, line):
+    def isComment(self, line):
 
-		line.strip(' ')
+        line.strip(' ')
 
-		if line[0] == self.commentSymbol:	# if line starts with the commentSymbol, it is a comment ..
-			return True			# 					.. don't process it.
+        if line[0] == self.commentSymbol:    # if line starts with the commentSymbol, it is a comment ..
+            return True            #                     .. don't process it.
 
-		else:
-			return False	# else: split the line at separators.
-
-
-	def load(self, filename):
-
-		score_file = open(filename, 'r') 
-		states = []
-
-		for line in score_file:
-
-			try:
-				if self.isComment(line):
-					continue
-
-				line = line.strip('\n').strip('\r').strip(' ')
-				x = line.split(self.lineSeparator)
-
-				if len(x) > 0:					# for example 1
-					start = x[0].strip(' ')
-
-				if len(x) == 1:
-					annot	 = ''
-					duration = ''
-
-				if len(x) == 2:
-					annot	 = x[1]
-					duration = ''
-
-				elif len(x) > 2:				# for example 3.
-					duration = x[1].strip(' ')
-					annot	 = x[2]
+        else:
+            return False    # else: split the line at separators.
 
 
-				if duration == '':
-					duration = '-1'
-	
-				states.append( st.state(start=start, duration=duration, annot=annot) )
+    def load(self, filename):
 
-			except:
-				if self.verbose > 0: print("# line not readable:", line)
+        score_file = open(filename, 'r') 
+        states = []
 
+        for line in score_file:
 
-		score_file.close()
+            try:
+                if self.isComment(line):
+                    continue
 
-		return states
+                line = line.strip('\n').strip('\r').strip(' ')
+                x = line.split(self.lineSeparator)
 
+                if len(x) > 0:                    # for example 1
+                    start = x[0].strip(' ')
 
-	def save(self, filename):
+                if len(x) == 1:
+                    annot     = ''
+                    duration = ''
 
-		print("# opening", filename, "to write ...")
-		score_file = open(filename, 'w')
-		string = '# start, duration, annotation\n'+self.__str__()
-		score_file.write(string + '\n')
-		score_file.close()
+                if len(x) == 2:
+                    annot     = x[1]
+                    duration = ''
 
-
-	def append(self, new_state=None, start=None, duration=None, annot=None):
-
-		if new_state == None:
-			new_state = st.state(start=start, duration=duration, annot=annot)
-
-		self.states.append(new_state)
-
-
-	def __str__(self):
-
-		return '\n'.join([str(state) for state in self.states])
+                elif len(x) > 2:                # for example 3.
+                    duration = x[1].strip(' ')
+                    annot     = x[2]
 
 
-	def select_by_function(self, function, **kwargs):
+                if duration == '':
+                    duration = '-1'
+    
+                states.append( st.state(start=start, duration=duration, annot=annot) )
 
-		selection = []
-		for state in self.states:
-			
-			if function(state, **kwargs):
-				selection.append(state)
-
-		score_select = object.__new__(type(self))
-		score_select.__init__(states=selection)
-		return score_select
+            except:
+                if self.verbose > 0: print("# line not readable:", line)
 
 
-	def intersect(self, other_score):
+        score_file.close()
 
-		intersection = []
-
-		for state in self.states:
-
-			section_j = state.intersect(other_score.states)
-			intersection.extend( section_j )
-	
-		return type(self)(states=intersection)
+        return states
 
 
-	def duration(self, annot=None):
+    def save(self, filename):
 
-		duration = 0.0
-		if annot == None:
-			duration = np.sum([state.duration for state in self.states])
-
-		else:
-			for state in self.states:
-				if state.annot == annot:
-					duration += state.duration
-
-		return duration
+        print("# opening", filename, "to write ...")
+        score_file = open(filename, 'w')
+        string = '# start, duration, annotation\n'+self.__str__()
+        score_file.write(string + '\n')
+        score_file.close()
 
 
-	def count(self, annot=None):
+    def append(self, new_state=None, start=None, duration=None, annot=None):
 
-		if annot == None:
-			count = len(self.states)
+        if new_state == None:
+            new_state = st.state(start=start, duration=duration, annot=annot)
 
-		else:
-			count = 0
+        self.states.append(new_state)
 
-			for state in self.states:
 
-				if state.annot == annot:
-					count += 1
+    def __str__(self):
 
-		return count
+        return '\n'.join([str(state) for state in self.states])
+
+
+    def select_by_function(self, function, **kwargs):
+
+        selection = []
+        for state in self.states:
+            
+            if function(state, **kwargs):
+                selection.append(state)
+
+        score_select = object.__new__(type(self))
+        score_select.__init__(states=selection)
+        return score_select
+
+
+    def intersect(self, other_score):
+
+        intersection = []
+
+        for state in self.states:
+
+            section_j = state.intersect(other_score.states)
+            intersection.extend( section_j )
+    
+        return type(self)(states=intersection)
+
+
+    def duration(self, annot=None):
+
+        duration = 0.0
+        if annot == None:
+            duration = np.sum([state.duration for state in self.states])
+
+        else:
+            for state in self.states:
+                if state.annot == annot:
+                    duration += state.duration
+
+        return duration
+
+
+    def count(self, annot=None):
+
+        if annot == None:
+            count = len(self.states)
+
+        else:
+            count = 0
+
+            for state in self.states:
+
+                if state.annot == annot:
+                    count += 1
+
+        return count
 
 
 
@@ -182,12 +182,12 @@ score = Score
 
 if __name__ == "__main__":
 
-	score_filename = '../../example/sample.csv'
+    score_filename = '../../example/sample.csv'
 
-	testscore = score(filename=score_filename)
+    testscore = score(filename=score_filename)
 
-	print(testscore)
-	
+    print(testscore)
+    
 
 
 
